6a8d5c9961a670df2e4bea3a114307e5097f78e1
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-06-18 10:27:52 -0600

    Update replaceAttr to work with linkoerr errWithAttrs error type

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"syscall"
 	"time"
 
+	"boot.dev/linko/internal/linkoerr"
 	"boot.dev/linko/internal/store"
 	pkgerr "github.com/pkg/errors"
 )
@@ -133,15 +134,19 @@ func replaceAttr(groups []string, a slog.Attr) slog.Attr {
 		if !ok {
 			return a
 		}
+
+		attrs := []slog.Attr{{Key: "message", Value: slog.StringValue(err.Error())}}
+		attrs = append(attrs, linkoerr.Attrs(err)...)
 		if stackErr, ok := errors.AsType[stackTracer](err); ok {
-			return slog.GroupAttrs("error", slog.Attr{
-				Key:   "message",
-				Value: slog.StringValue(stackErr.Error()),
-			}, slog.Attr{
-				Key:   "stack_trace",
-				Value: slog.StringValue(fmt.Sprintf("%+v", stackErr.StackTrace())),
-			})
+			attrs = append(
+				attrs,
+				slog.Attr{
+					Key:   "stack_trace",
+					Value: slog.StringValue(fmt.Sprintf("%+va", stackErr.StackTrace())),
+				},
+			)
 		}
+		return slog.GroupAttrs("error", attrs...)
 	}
 	return a
 }
