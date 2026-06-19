c3b56b5b3a6325b33a9891722a8b868e7eac7d80
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-06-19 10:11:24 -0600

    Add build attrs to logger

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"syscall"
 	"time"
 
+	"boot.dev/linko/internal/build"
 	"boot.dev/linko/internal/linkoerr"
 	"boot.dev/linko/internal/store"
 	pkgerr "github.com/pkg/errors"
@@ -31,6 +32,10 @@ func main() {
 
 func run(ctx context.Context, cancel context.CancelFunc, httpPort int, dataDir string) int {
 	logger, closeLogger, err := initializeLogger(os.Getenv("LINKO_LOG_FILE"))
+	logger = logger.With(
+		slog.String("git_sha", build.GitSHA),
+		slog.String("build_time", build.BuildTime),
+	)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to initialize logger: %v\n", err)
 		return 1
