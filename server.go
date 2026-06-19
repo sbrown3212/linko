a2d088f6cea31617f996dbfcee1a845cfb457c27
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2026-06-19 12:36:07 -0600

    Add spyResponseWriter struct and response related logger attributes to requestLogger
    Implement spyResponseWriter struct and methods. Implement response_status and response_body_bytes logger attributes to requestLogger middleware

--- server.go
+++ server.go
@@ -75,13 +75,34 @@ func (r *spyReadCloser) Read(p []byte) (int, error) {
 	return n, err
 }
 
+type spyResponseWriter struct {
+	http.ResponseWriter
+	bytesWritten int
+	statusCode   int
+}
+
+func (w *spyResponseWriter) Write(p []byte) (int, error) {
+	if w.statusCode == 0 {
+		w.statusCode = http.StatusOK
+	}
+	n, err := w.ResponseWriter.Write(p)
+	w.bytesWritten += n
+	return n, err
+}
+
+func (w *spyResponseWriter) WriteHeader(statusCode int) {
+	w.statusCode = statusCode
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
 func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			start := time.Now()
 			spyReader := &spyReadCloser{ReadCloser: r.Body}
 			r.Body = spyReader
-			next.ServeHTTP(w, r)
+			spyWriter := &spyResponseWriter{ResponseWriter: w}
+			next.ServeHTTP(spyWriter, r)
 
 			logger.Info("Served request",
 				slog.String("method", r.Method),
@@ -89,6 +110,8 @@ func requestLogger(logger *slog.Logger) func(http.Handler) http.Handler {
 				slog.String("client_ip", r.RemoteAddr),
 				slog.Duration("duration", time.Since(start)),
 				slog.Int("request_body_bytes", spyReader.bytesRead),
+				slog.Int("response_status", spyWriter.statusCode),
+				slog.Int("response_body_bytes", spyWriter.bytesWritten),
 			)
 		})
 	}
